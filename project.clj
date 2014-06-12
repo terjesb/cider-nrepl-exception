929da16923df690ee2d87c8040da2ed262d444e2
Terje Sten Bjerkseth <terje@ceteo.no>
2014-06-12 10:20:54 +0200

    Add lein immutant plugin

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]]
-  :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]]
+  :plugins [[lein-immutant "1.2.1"]
+            [cider/cider-nrepl "0.7.0-SNAPSHOT"]
+            ]
   :repl-options {:nrepl-middleware [cider.nrepl.middleware.classpath/wrap-classpath
                                     cider.nrepl.middleware.complete/wrap-complete
                                     cider.nrepl.middleware.info/wrap-info
